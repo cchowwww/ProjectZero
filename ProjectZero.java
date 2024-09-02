acf5081446334c6287ec71ed13949386a3cbe7b0
Cristy Chow <cristychow@cpp.edu>
2024-09-02 14:07:45 -0700

    First Commit

---
+++ ProjectZero.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class ProjectZero {
+    public static void main(String[] args) {
+        Scanner obj= new Scanner(System.in);
+		double score1, score2, score3;
+		double totalScore;
+		
+		System.out.println("Enter your first test score: ");
+		score1= obj.nextDouble();
+		System.out.println("Enter your second test score: ");
+		score2= obj.nextDouble();
+		System.out.println("Enter your third test score: ");
+		score3= obj.nextDouble();
+		
+		totalScore = score1 + score2 + score3;
+		System.out.println("Test scores: " + score1 + ", " + score2 + ", " + score3);
+		System.out.println("Average of scores: " + totalScore/3);
+		if (totalScore/3 > 95) 
+		{
+			System.out.println("Congratuations on your high test scores");
+		}
+
+    }
+    
+}
